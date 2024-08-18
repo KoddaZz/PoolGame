08a0380a200f3456073d24a3a79de843412b34ce
KoddaZz <jules.fuselier0401@gmail.com>
2024-08-18 18:15:37 +0200

    update
    Co-authored-by: StarFr0zen <StarFr0zen@users.noreply.github.com>

---
+++ Class/white_ball.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+"""
+@Author :   StarFr0zen
+            KoddaZz
+
+white_ball.py : Script correspondant à la gestion de la boule blanche
+"""
+
+RAYON = 10 # taille balle
+LONGUEUR_ECRAN = 1000 # taille longueur écran de jeu (x)
+LARGEUR_ECRAN = 500 # taille largeur écran de jeu (y)
+
+fenetre = pygame.display.set_mode((LONGUEUR_ECRAN, LARGEUR_ECRAN))
+fenetre.fill([0, 0, 0])
+
+#class White_Ball:
